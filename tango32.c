b3f8c2b8586b624759c4d90a9c1eec47efd25078
dcaclan <mr.shaikdastgeer@gmail.com>
2026-01-28 18:14:09 +0530

    Update tango32.c

--- tango32.c
+++ tango32.c
@@ -6,20 +6,20 @@
 #include <linux/compat.h>
 #include "tango32.h"
 
-static bool is_32(void) { return test_thread_flag(TIF_32BIT); }
-
 static long tango32_ioctl(struct file *file, unsigned int cmd, unsigned long arg) {
     void __user *argp = (void __user *)arg;
     struct tango32_abi_version abi = { .major = TANGO32_ABI_MAJOR, .minor = TANGO32_ABI_MINOR };
 
-    if (is_32()) return -EINVAL;
+    // Kernel 6.6 compatibility: Direct thread flag check
+    if (test_thread_flag(TIF_32BIT)) return -EINVAL;
 
     if (cmd == TANGO32_GET_VERSION) {
         set_thread_flag(TIF_32BIT);
         bool c = in_compat_syscall();
         clear_thread_flag(TIF_32BIT);
         if (!c) return -EIO;
-        return copy_to_user(argp, &abi, sizeof(abi)) ? -EFAULT : 0;
+        if (copy_to_user(argp, &abi, sizeof(abi))) return -EFAULT;
+        return 0;
     }
 
     return -ENOIOCTLCMD;
