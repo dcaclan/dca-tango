06fd9262d7726a1e6c48611be9e100593e999daa
dcaclan <mr.shaikdastgeer@gmail.com>
2026-01-28 18:59:10 +0530

    Update tango32.c

--- tango32.c
+++ tango32.c
@@ -6,22 +6,12 @@
 #include <linux/uaccess.h>
 #include <linux/compat.h>
 #include <linux/sched.h>
-#include "tango32.h"
 
 static long tango32_ioctl(struct file *file, unsigned int cmd, unsigned long arg) {
-    void __user *argp = (void __user *)arg;
-    struct tango32_abi_version abi = { .major = TANGO32_ABI_MAJOR, .minor = TANGO32_ABI_MINOR };
-
+    // 32-bit check logic
     if (test_thread_flag(TIF_32BIT)) return -EINVAL;
-
-    if (cmd == TANGO32_GET_VERSION) {
-        set_thread_flag(TIF_32BIT);
-        bool c = in_compat_syscall();
-        clear_thread_flag(TIF_32BIT);
-        if (!c) return -EIO;
-        if (copy_to_user(argp, &abi, sizeof(abi))) return -EFAULT;
-        return 0;
-    }
+    
+    // Minimal IOCTL to avoid struct padding issues
     return -ENOIOCTLCMD;
 }
 
@@ -38,11 +28,17 @@ static struct miscdevice t_dev = {
     .mode = 0666,
 };
 
-static int __init t_init(void) { return misc_register(&t_dev); }
-static void __exit t_exit(void) { misc_deregister(&t_dev); }
+static int __init t_init(void) {
+    return misc_register(&t_dev);
+}
+
+static void __exit t_exit(void) {
+    misc_deregister(&t_dev);
+}
 
 module_init(t_init);
 module_exit(t_exit);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("DCA");
+MODULE_INFO(intree, "Y");
